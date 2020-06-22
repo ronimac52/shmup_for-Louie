6c92cf133d61dfd32a959c4fbaed8c2dacf4028a
Ronan <ronanmusic@btinternet.com>
2020-06-22 17:54:44 +0100

    added sprites class

--- shmup.py
+++ shmup.py
@@ -12,7 +12,14 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-
+#define sprite class
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
 # initialise pygame and create window
 pygame.init()
 pygame.mixer.init()  # for sound
